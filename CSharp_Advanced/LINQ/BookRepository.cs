097f64268d5fc83e3ff577b0bcd5ea6161102e40
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-12 23:19:54 +0200

    LINQ

---
+++ CSharp_Advanced/LINQ/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book {Title = "Title 1" , Price = 7},
+                new Book {Title = "Title 2" , Price = 7.2f},
+                new Book {Title = "Title 3" , Price = 9},
+                new Book {Title = "Title 4" , Price = 17}
+            };
+        }
+    }
+}
---
+++ CSharp_Advanced/LINQ/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        //Lesson 6 : LINQ
+        static void Main(string[] args)
+        {
+            //LINQ Extension Methods
+            var books = new BookRepository().GetBooks();
+
+            var cheapBooks = books.Where(x => x.Price < 10).Select(x => x.Title);
+            foreach (var book in cheapBooks)
+            {
+                //Console.WriteLine(book.Title);
+                Console.WriteLine(book);
+            }
+
+            //LINQ Queries
+            var cheapBooks2 = from b in books
+                              where b.Price < 10
+                              select b.Title;
+
+            foreach (var book in cheapBooks2)
+            {
+                //Console.WriteLine(book.Title);
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("-----------");
+            //Return a single book
+
+            //var singleBook = books.Single(b => b.Title == "Title 3");
+            var singleBook = books.SingleOrDefault(b => b.Title == "Title 3");
+            Console.WriteLine(singleBook.Title);
+
+            //Skip data 
+            Console.WriteLine("-----------");
+            var chosenBooks = books.Skip(2);
+            foreach (var book in chosenBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
