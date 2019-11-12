3c7bea3d5e9d053ca1518fad913257ac802b2308
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-12 20:17:38 +0200

    Lambda expressions

---
+++ CSharp_Advanced/Lambda_Expressions/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda_Expressions
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book{Title="Title 1",Price=6},
+                new Book{Title="Title 2",Price=8},
+                new Book{Title="Title 3",Price=97}
+            };
+        }
+    }
+}
---
+++ CSharp_Advanced/Lambda_Expressions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lambda_Expressions
+{
+    class Program
+    {
+        // Lesson 3 : Lambda Expressions
+        static void Main(string[] args)
+        {
+            //ex1
+            //return square with lambda
+
+            //Func<int,int> square = number => number * number;
+            //Console.WriteLine(square(5));
+
+            //ex2
+            //const int factor = 5;
+            //Func<int, int> multiplier = number => number * factor;
+            //Console.WriteLine(multiplier(6));
+
+            //ex3
+            var books = new BookRepository().GetBooks();
+            var expensiveBooks = books.FindAll(x => x.Price > 10);
+            foreach (var item in expensiveBooks)
+            {
+                Console.WriteLine(item.Title);
+            }
+          
+        }
+    }
+}
