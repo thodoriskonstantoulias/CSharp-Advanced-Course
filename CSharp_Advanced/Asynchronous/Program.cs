a152f955d2d58c7efa5ef6c7d7c20c263896ef52
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-13 22:44:05 +0200

    Asynchronous Programming

---
+++ CSharp_Advanced/Asynchronous/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Asynchronous
+{
+    class Program
+    {
+        //Lesson 10 : Asynchronous Programming
+        static async Task Main(string[] args)
+        {
+            await DownloadHtmlAsync("http://msdn.microsoft.com");
+            Console.WriteLine("Finished");
+        }
+        public static async Task DownloadHtmlAsync(string url)
+        {
+            var client = new WebClient();
+            var html = await client.DownloadStringTaskAsync(url);
+            using (var streamReader = new StreamWriter(@"D:\Users\results.html"))
+            {
+                await streamReader.WriteAsync(html);
+            }
+        } 
+    }
+}
