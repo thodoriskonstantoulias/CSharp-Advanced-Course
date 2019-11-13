82f71fb9294642c041760349915ae73c80101bf4
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-13 21:07:17 +0200

    Nulls

---
+++ CSharp_Advanced/Nulls/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nulls
+{
+    class Program
+    {
+        //Lesson 7 : Nullable types
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            Console.WriteLine(date.GetValueOrDefault());
+            Console.WriteLine(date.HasValue);
+
+            DateTime date2 = date ?? DateTime.Today;
+            Console.WriteLine(date2);
+
+        }
+    }
+}
