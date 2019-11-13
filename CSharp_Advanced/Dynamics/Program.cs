40d7fb457fca552d2b9c475435ce82b45f9e478c
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-13 21:16:39 +0200

    Dynamics

---
+++ CSharp_Advanced/Dynamics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dynamics
+{
+    class Program
+    {
+        //Lesson 8 : Dynamics
+        static void Main(string[] args)
+        {
+            dynamic name = "Ted";
+            name = 10;
+            Console.WriteLine(name);
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+            Console.WriteLine(c);
+
+            int i = 5;
+            dynamic d = i;
+            Console.WriteLine(d);
+        }
+    }
+}
